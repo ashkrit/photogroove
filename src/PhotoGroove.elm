e10a37bbd5dcfbb91aa838f1e07bbe47b96b51c2
Ashkrit <ashkrit@gmail.com>
2020-08-23 23:08:35 +0800

    Add photo selction feaure

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -11,19 +11,32 @@ urlPrefix =
 view model =
     div [ class "content" ]
         [ h1 [] [ text "Photo Groove" ]
-        , div [ id "thumbnails" ] (List.map viewThumpnail model)
+        , div [ id "thumbnails" ] (List.map (\photo -> viewThumpnail model.selectedUrl photo) model.photos)
         ]
 
 
-viewThumpnail thumb =
-    img [ src (urlPrefix ++ thumb.url) ] []
+viewThumpnail selectedUrl thumb =
+    if selectedUrl == thumb.url then
+        img
+            [ src (urlPrefix ++ thumb.url)
+            , class "selected"
+            ]
+            []
+
+    else
+        img [ src (urlPrefix ++ thumb.url) ]
+            []
 
 
 intialModel =
-    [ { url = "1.jpeg" }
-    , { url = "2.jpeg" }
-    , { url = "3.jpeg" }
-    ]
+    { photos =
+        [ { url = "1.jpeg" }
+        , { url = "2.jpeg" }
+        , { url = "3.jpeg" }
+        ]
+    , selectedUrl = "1.jpeg"
+    }
+
 
 main =
     view intialModel
