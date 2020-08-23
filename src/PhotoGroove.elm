2e4e1eecca87c133496f6e5de88d9e6d9e3e037e
Ashkrit <ashkrit@gmail.com>
2020-08-23 22:39:42 +0800

    Basic example of simple view

---
+++ src/PhotoGroove.elm
@@ -0,0 +1,26 @@
+module PhotoGroove exposing (main)
+
+import Html exposing (div, h1, img, text)
+import Html.Attributes exposing (..)
+
+
+view model =
+    div [ class "content" ]
+        [ h1 [] [ text "Photo Groove - New" ]
+        , div [ id "thumbnails" ]
+            [ img [ src "http://elm-in-action.com/1.jpeg" ] []
+            , img [ src "http://elm-in-action.com/2.jpeg" ] []
+            , img [ src "http://elm-in-action.com/3.jpeg" ] []
+            ]
+        ]
+
+
+intialModel =
+    [ { url = "1.jpeg" }
+    , { url = "2.jpeg" }
+    , { url = "3.jpeg" }
+    ]
+
+
+main =
+    view intialModel
