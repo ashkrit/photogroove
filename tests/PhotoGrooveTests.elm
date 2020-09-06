8c007509a12385268906932a34aada0fe1ed5041
Ashkrit <ashkrit@gmail.com>
2020-09-06 16:46:28 +0800

    dynamic JSON

--- tests/PhotoGrooveTests.elm
+++ tests/PhotoGrooveTests.elm
@@ -1,8 +1,8 @@
 module PhotoGrooveTests exposing (..)
 
 import Expect exposing (Expectation)
-import Html.Attributes exposing (title)
-import Json.Decode exposing (decodeString)
+import Json.Decode as Decode exposing (decodeString,decodeValue)
+import Json.Encode as Encode
 import PhotoGroove exposing (..)
 import Test exposing (..)
 
@@ -23,6 +23,20 @@ nicedecoderTest =
         \_ ->
             """{"url":"fruits.com" , "size":5 }"""
                 |> decodeString PhotoGroove.photoDecoder
+                |> Result.map .title
+                |> Expect.equal
+                    (Ok "(untitle)")
+
+
+dynamicjsondecoderTest : Test
+dynamicjsondecoderTest =
+    test "Title default to (untitled) using dynamic json" <|
+        \_ ->
+            [ ( "url", Encode.string "fruits.com" )
+            , ( "size", Encode.int 5 )
+            ]
+                |> Encode.object
+                |> decodeValue PhotoGroove.photoDecoder
                 |> Result.map .title
                 |> Expect.equal
                     (Ok "(untitle)")
