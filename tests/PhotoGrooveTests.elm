fc6d0cfa9b756186824e791dd8976ba8c641ca72
Ashkrit <ashkrit@gmail.com>
2020-09-06 16:38:24 +0800

    short syntax example

--- tests/PhotoGrooveTests.elm
+++ tests/PhotoGrooveTests.elm
@@ -1,6 +1,7 @@
 module PhotoGrooveTests exposing (..)
 
 import Expect exposing (Expectation)
+import Html.Attributes exposing (title)
 import Json.Decode exposing (decodeString)
 import PhotoGroove exposing (..)
 import Test exposing (..)
@@ -14,3 +15,14 @@ decoderTest =
                 |> decodeString PhotoGroove.photoDecoder
                 |> Expect.equal
                     (Ok { title = "(untitle)", url = "fruits.com", size = 5 })
+
+
+nicedecoderTest : Test
+nicedecoderTest =
+    test "Title default to (untitled) using nice syntax" <|
+        \_ ->
+            """{"url":"fruits.com" , "size":5 }"""
+                |> decodeString PhotoGroove.photoDecoder
+                |> Result.map .title
+                |> Expect.equal
+                    (Ok "(untitle)")
